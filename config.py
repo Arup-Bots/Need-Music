77de6e06458a06d1717ef6adbdb41966e0f2370b
Arup Mandal <110271890+cyberboyarup@users.noreply.github.com>
2022-09-16 09:25:55 +0530

    Arup Mandal

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+
+API_ID = os.getenv('API_ID',16507294)                            # You can get this from -->  https://my.telegram.org/auth  
+API_HASH = os.getenv('API_HASH','b696628fbcd23c8f6bc70ddb82484694')               # You can get this from -->  https://my.telegram.org/auth
+BOT_TOKEN = os.getenv('BOT_TOKEN','5618162443:AAEWJiVO16Uco_i7ZnORP5Dmvgj2LXhWfLc')           # You can get this from --> @BotFather
+BOT_NAME = os.getenv('BOT_NAME','themusicdownloaderbot_bot')           #Username of your bot
+GENIUS_API = os.getenv('GENIUS_API','GBd9UuDGjldDyuT5VKS7-8YNuZnoF5cGjGknf0nA9eEkh4W8o4vBRkHK1maZiVSz')        # You can get this from --> https://docs.genius.com/
